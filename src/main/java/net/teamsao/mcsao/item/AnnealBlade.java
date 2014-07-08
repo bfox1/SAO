7607fa0c2df36242c4cc607d33e642a60d1ffacf
Christian B <aweschris@hotmail.com>
2014-07-08 02:19:11 -0400

    Check description; TL, so it DR
    I don't even know what I did. GitHub is making me commit the same work 20 million times T_T Halp.

--- src/main/java/net/teamsao/mcsao/item/AnnealBlade.java
+++ src/main/java/net/teamsao/mcsao/item/AnnealBlade.java
@@ -20,27 +20,18 @@ public class AnnealBlade extends ItemSword {
 		this.setUnlocalizedName("AnnealBlade");
 		this.setTextureName(Reference.MODID + ":" + getUnlocalizedName().substring(5));
 		this.setCreativeTab(SCreativeTab.SAO);
-
 	}
 	
 	@Override
-    public ItemStack onItemRightClick(ItemStack par1, World par2, EntityPlayer par3)
-    {
+    public ItemStack onItemRightClick(ItemStack par1, World par2, EntityPlayer par3) {
         ItemStack sword = new ItemStack(SItem.AnnealBladePowered);
-
         par3.setItemInUse(par1, this.getMaxItemUseDuration(par1));
-        if(!par2.isRemote)
-        {
-        	if(par3.isSneaking())
-        	{
+        if(!par2.isRemote && par3.isSneaking()) {
 			sword.setItemDamage(par1.getItemDamage());
 			sword.addEnchantment(Enchantment.unbreaking, 2);
 			sword.addEnchantment(Enchantment.looting, 2);
-
             return sword;
-        	}
         }
-        
 
         return par1;
     }
