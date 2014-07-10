fcd7e8f4643da0f27b67f8660d110c8e4cdd204f
bfox1 <robert.gamiz@yahoo.com>
2014-07-09 22:01:18 -0700

    uncommitted commits

---
+++ src/main/java/net/teamsao/mcsao/item/RabbitMeat.java
@@ -0,0 +1,19 @@
+package net.teamsao.mcsao.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
+import net.teamsao.mcsao.help.ReferenceHelper;
+import net.teamsao.mcsao.lib.SCreativeTab;
+
+/**
+ * Created by bfox1 on 7/9/2014.
+ */
+public class RabbitMeat extends ItemFood {
+        public RabbitMeat(int heartsHeal, float saturation, boolean wolfLike, String name)
+        {
+            super(heartsHeal, saturation, wolfLike);
+            this.setUnlocalizedName(name);
+            this.setTextureName(ReferenceHelper.setItemName(this));
+            this.setCreativeTab(SCreativeTab.SAO_Food);
+        }
+}
--- src/main/java/net/teamsao/mcsao/lib/SCreativeTab.java
+++ src/main/java/net/teamsao/mcsao/lib/SCreativeTab.java
@@ -13,11 +13,13 @@ public class SCreativeTab {
 		public static CreativeTabs SAO_Tools;
 		
 		public static CreativeTabs SAO_Blocks;
-		
-		public static void registerCreativeTab()
+        public static CreativeTabs SAO_Food;
+
+    public static void registerCreativeTab()
 		{
 			SAO = new SAOTabsManager(CreativeTabs.getNextID(), "SAO");
 			SAO_Tools = new SAOTabsManager(CreativeTabs.getNextID(), "SAOTools");
 			SAO_Blocks = new SAOTabsManager(CreativeTabs.getNextID(), "SAOBlocks");
+            SAO_Food = new SAOTabsManager(CreativeTabs.getNextID(), "SAOFoods");
 		}
 }
