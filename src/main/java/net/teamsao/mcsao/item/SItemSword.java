1c0b8607c33a180186b852177e7be495bbb00bab
igraham <ianrgraham@gmail.com>
2014-08-09 02:46:25 -0400

    Added Textures and Organized SItemSword

--- src/main/java/net/teamsao/mcsao/item/SItemSword.java
+++ src/main/java/net/teamsao/mcsao/item/SItemSword.java
@@ -1,5 +1,8 @@
 package net.teamsao.mcsao.item;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
@@ -8,24 +11,26 @@ import net.minecraft.world.World;
 /**
  * Created by Bobby on 7/8/2014.
  */
-public abstract class SItemSword extends ItemSword {
-    public SItemSword(ToolMaterial p_i45356_1_) {
-        super(p_i45356_1_);
-    }
+public abstract class SItemSword extends ItemSword
+{
+	public SItemSword(ToolMaterial p_i45356_1_)
+	{
+		super(p_i45356_1_);
+	}
 
-    @Override
-    public ItemStack onItemRightClick(ItemStack par1ItemStack, World world, EntityPlayer player)
-    {
-        player.setItemInUse(par1ItemStack, this.getMaxItemUseDuration(par1ItemStack));
-        //ItemStack sword = new ItemStack(SAOItems.EbonDagger);
-        if(!world.isRemote)
-        {
-            if(player.isSneaking())
-            {
-                //sword.setItemDamage(par1ItemStack.getItemDamage());
-               // return sword;
-            }
-        }
-        return par1ItemStack;
-    }
+	@Override
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World world, EntityPlayer player)
+	{
+		player.setItemInUse(par1ItemStack, this.getMaxItemUseDuration(par1ItemStack));
+		//ItemStack sword = new ItemStack(SAOItems.EbonDagger);
+		if(!world.isRemote)
+		{
+			if(player.isSneaking())
+			{
+				//sword.setItemDamage(par1ItemStack.getItemDamage());
+				// return sword;
+			}
+		}
+		return par1ItemStack;
+	}
 }
