f536373216d78d93a6fa48fbedbb851e598bdf37
igraham <ianrgraham@gmail.com>
2014-08-28 06:14:51 -0400

    Added Cone below floor 1
    The shape of the cone is very flat, seeing as how it expands all over
    floor 1. I might be able to do something else, depending on the
    recommendations of people, but this should suffice for "padding" dirt
    between the player and the void.

--- src/main/java/net/teamsao/mcsao/dimension/SAOBiomeGen.java
+++ src/main/java/net/teamsao/mcsao/dimension/SAOBiomeGen.java
@@ -61,31 +61,50 @@ public class SAOBiomeGen extends BiomeGenBase
 		this.genVoidTerrain(world, random, blocks, metadata, x, z, noiseGenSeed);
 	}
 	
+	/**
+	 * Used as a semi-weird way to make a cone underneath floor 1. This works specifically only if you use a cylindrical shape for whatever structure and if
+	 * the dimensions to the cylinder and placement are exactly like floor 1. You can probably use this method as a base for other methods if nothing else.
+	 * @param distance
+	 * @param blockHeight
+	 * @return true if the distance and block height are correct for the cone shape.
+	 */
+	private boolean isWithinCone(int distance, int blockHeight)
+	{
+		double dist = (double)distance;
+		double height = (double)blockHeight;
+		double mult = 39.0D / 495.0D;
+		if(height < 40 && height >= ((dist * mult)))
+		{
+			return true;
+		}
+		return false;
+	}
+	
 	public void genVoidTerrain(World world, Random random, Block[] blocks, byte[] metadata, int x, int z, double noiseGenSeed)
     {
 		int distance = StructureGenHelper.distance2D(x, z);
 		int chunkX = x & 15;
         int chunkZ = z & 15;
         int chunkHeight = blocks.length / 256;
-		if(distance <= 500 && distance >= 497)
+		if(distance <= 500 && distance >= 496)
 		{
 			for (int blockHeight = 255; blockHeight >= 0; --blockHeight)
 	        {
 				int blockIndex = (chunkZ * 16 + chunkX) * chunkHeight + blockHeight;
 	        	metadata[blockIndex] = 0;
-	        	int wallEnd = ((4-(500 - distance)) * 20)+40;
+	        	int wallEnd = ((5-(500 - distance)) * 20)+40;
 	        	int wallStart = wallEnd-20;
-	        	if(blockHeight >= wallStart && blockHeight <= wallEnd)
+	        	if(blockHeight >= wallStart && blockHeight < wallEnd)
 	        	{
 	        		blocks[blockIndex] = this.wallBlock;
 	        	}
 	        }
 		}
-		if(distance < 497)
+		if(distance < 496)
         {
-	        Block block = this.topBlock;
+	        Block aincradGrass = this.topBlock;
 	        byte blockData = (byte)(this.field_150604_aj & 255);
-	        Block block1 = this.fillerBlock;
+	        Block aincradDirt = this.fillerBlock;
 	        int noiseGenRand = (int)Math.pow(noiseGenSeed / 3.0D + 3.0D, 2);
 	        int blockMaxHeight = noiseGenRand + 40;
 	        for (int blockHeight = 255; blockHeight >= 0; --blockHeight)
@@ -94,11 +113,15 @@ public class SAOBiomeGen extends BiomeGenBase
 	        	metadata[blockIndex] = 0;
 	        	if(blockHeight == blockMaxHeight)
 	        	{
-	        		blocks[blockIndex] = block;
+	        		blocks[blockIndex] = aincradGrass;
 	        	}
 	        	else if(blockHeight < blockMaxHeight && blockHeight >= 40)
 	        	{
-	        		blocks[blockIndex] = block1;
+	        		blocks[blockIndex] = aincradDirt;
+	        	}
+	        	else if(blockHeight < 40 && blockHeight >=0 && isWithinCone(distance, blockHeight))
+	        	{
+	        		blocks[blockIndex] = aincradDirt;
 	        	}
 	        	else
 	        	{
