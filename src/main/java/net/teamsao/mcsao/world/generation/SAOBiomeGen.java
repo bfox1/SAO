76c386c78daf6993e408654ee0fcbea48dfd70a5
Ian Graham <ianrgraham@gmail.com>
2014-10-01 21:38:32 -0400

    Added in 100 Floors as Templates
    Floors are now represented in data as Floor Points, which store lots of
    data about how the floor gets generated in world gen. There are a few
    helper methods to make sure that changing one of them excessively does
    not mess anything up. These starter points and their data are then used
    to generate the floors in world gen. We can also use them to add the
    towers and dungeons in, based on the center point and the size of the
    floors.

--- src/main/java/net/teamsao/mcsao/world/generation/SAOBiomeGen.java
+++ src/main/java/net/teamsao/mcsao/world/generation/SAOBiomeGen.java
@@ -1,11 +1,13 @@
 package net.teamsao.mcsao.world.generation;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.teamsao.mcsao.helper.FloorPoint;
 import net.teamsao.mcsao.helper.StructureGenHelper;
 import net.teamsao.mcsao.init.SAOBlocks;
 
@@ -16,10 +18,11 @@ public class SAOBiomeGen extends BiomeGenBase
 	private int wallThickness = 5;
 	private int wallStart = 999;
 	private int wallEnd = wallStart+wallThickness;
+	private int floorSeparation = ((wallStart*2)+512);
+	private int floorsEnd = (floorSeparation*10)+wallEnd;
 	
-	private int floor2HeightStart = 40+wallSegmentHeight*wallThickness;
-	private int floor2WallStart = wallEnd;
-	private int floor2WallEnd = floor2WallStart + wallThickness;
+	private ArrayList<FloorPoint> floorCenters = getFloorCenterPoints();
+	private FloorPoint currentFloor;
 	/**
 	 * 
 	 * Attempts to create a biome more friendly for building with no blocks anywhere except the portal.
@@ -81,90 +84,151 @@ public class SAOBiomeGen extends BiomeGenBase
 		return false;
 	}
 	
+	private ArrayList<FloorPoint> getFloorCenterPoints()
+	{
+		ArrayList<FloorPoint> centerPoints = new ArrayList<FloorPoint>();
+		for(int x = 0; x < 10; x++)
+		for(int z = 0; z < 10; z++)
+		{
+			FloorPoint point = new FloorPoint((x*10)+(z+1), x*floorSeparation, 40, z*floorSeparation, 
+				wallStart, wallThickness, 1.4D);
+			centerPoints.add(point);
+		}
+		return centerPoints;
+	}
+	
+	private void setFloorPointSize(int floorNumber, int radius, int wallThickness)
+	{
+		if(floorNumber <= 0 || floorNumber >= 100) return;
+		FloorPoint floor = floorCenters.get(floorNumber-1);
+		floor.setFloorRadius(radius);
+		floor.setWallThickness(wallThickness);
+		calculateFloorSeparation();
+	}
+	
+	private void setFloorPointAmplitude(int floorNumber, int floorAmplitude)
+	{
+		if(floorNumber <= 0 || floorNumber >= 100) return;
+		FloorPoint floor = floorCenters.get(floorNumber-1);
+		floor.setFloorAmplitude(floorAmplitude);
+	}
+	
+	/**
+	 * Whenever an update to one of the floors has been made and the position or size has changed, this method can be 
+	 * used to iterate through all of the floor center points and correct the differences to ensure that the distance 
+	 * between each floor's wall X and Z extreme boundaries is at least 512 blocks (32 chunks). This will most likely 
+	 * make some floors look like they're out of place, but it should prevent them all from being visible from each
+	 * others' boundaries.
+	 */
+	private void calculateFloorSeparation()
+	{
+		FloorPoint floor, floorWest, floorEast, floorNorth, floorSouth;
+		for(int x = 0; x < 10; x++)
+		for(int z = 0; z < 10; z++)
+		{
+			floor = floorCenters.get((x*10)+z);
+			if(z > 0)
+			{
+				floorSouth = floorCenters.get((x*10)+(z-1));
+				if(floorSouth.getPosZBoundary() + 512 >= floor.getNegZBoundary())
+				{
+					floor.setZ(floorSouth.getPosZBoundary() + 512 + floor.getFloorRadius());
+				}
+			}
+			if(z < 9)
+			{
+				floorNorth = floorCenters.get((x*10)+(z+1));
+				if(floorNorth.getNegZBoundary() - 512 <= floor.getPosZBoundary())
+				{
+					floorNorth.setZ(floor.getPosZBoundary() + 512 + floorNorth.getFloorRadius());
+				}
+			}
+			if(x > 0)
+			{
+				floorWest = floorCenters.get(((x-1)*10)+z);
+				if(floorWest.getPosXBoundary() + 512 >= floor.getNegXBoundary())
+				{
+					floor.setX(floorWest.getPosXBoundary() + 512 + floor.getFloorRadius());
+				}
+			}
+			if(x < 9)
+			{
+				floorEast = floorCenters.get(((x+1)*10)+z);
+				if(floorEast.getNegXBoundary() - 512 >= floor.getPosXBoundary())
+				{
+					floorEast.setX(floor.getPosXBoundary() + 512 + floorEast.getFloorRadius());
+				}
+			}
+		}
+	}
+	
+	private int getCurrentFloorNumber(int x, int z)
+	{
+		for(FloorPoint point : floorCenters)
+		{
+			if(x >= point.getNegXBoundary() && x <= point.getPosXBoundary() && 
+					z >= point.getNegZBoundary() && z <= point.getPosZBoundary())
+			{
+				return point.getFloorNumber();
+			}
+		}
+		return 0;
+	}
+	
 	public void genVoidTerrain(World world, Random random, Block[] blocks, byte[] metadata, int x, int z, double noiseGenSeed)
     {
-		int distance = StructureGenHelper.distance2D(x, z);
-		int chunkX = x & 15;
-        int chunkZ = z & 15;
-        int chunkHeight = blocks.length / 256;
-		if(distance <= wallEnd && distance > wallStart)
+		if(x <= floorsEnd && x >= -wallEnd && z <= floorsEnd && z >= -wallEnd)
 		{
-			int wallTop = ((wallThickness-(wallEnd - distance)) * wallSegmentHeight)+40;
-        	int wallBottom = wallTop-wallSegmentHeight;
-        	
-			for (int blockHeight = 255; blockHeight >= 0; --blockHeight)
+			int floorNumber = getCurrentFloorNumber(x, z);
+			if(floorNumber == 0) return;
+			currentFloor = floorCenters.get(floorNumber-1);
+			int distance = StructureGenHelper.distance2D(x, z, currentFloor.getX(), currentFloor.getZ());
+			int chunkX = x & 15;
+	        int chunkZ = z & 15;
+	        int chunkHeight = blocks.length / 256;
+	        int noiseGenRand = (int)Math.pow(noiseGenSeed / 3.0D + 3.0D, currentFloor.getFloorAmplitude());
+			if(distance <= wallEnd && distance > wallStart)
+			{
+				int wallTop = ((wallThickness-(wallEnd - distance)) * wallSegmentHeight)+40;
+	        	int wallBottom = wallTop-wallSegmentHeight;
+	        	
+				for (int blockHeight = 255; blockHeight >= 0; --blockHeight)
+		        {
+					int blockIndex = (chunkZ * 16 + chunkX) * chunkHeight + blockHeight;
+		        	metadata[blockIndex] = 0;
+		        	if(blockHeight <= wallTop && blockHeight >= wallBottom)
+		        	{
+		        		blocks[blockIndex] = this.wallBlock;
+		        	}
+		        }
+			}
+			else if(distance <= wallStart)
 	        {
-				int blockIndex = (chunkZ * 16 + chunkX) * chunkHeight + blockHeight;
-	        	metadata[blockIndex] = 0;
-	        	if(blockHeight <= wallTop && blockHeight >= wallBottom)
-	        	{
-	        		blocks[blockIndex] = this.wallBlock;
-	        	}
-	        }
-		}
-		else if(distance <= wallStart)
-        {
-	        Block aincradGrass = this.topBlock;
-	        byte blockData = (byte)(this.field_150604_aj & 255);
-	        Block aincradDirt = this.fillerBlock;
-	        int noiseGenRand = (int)Math.pow(noiseGenSeed / 3.0D + 3.0D, 1.4D);
-	        int blockMaxHeight = noiseGenRand + 40;
-	        for (int blockHeight = 255; blockHeight >= 0; --blockHeight)
-	        {
-	        	int blockIndex = (chunkZ * 16 + chunkX) * chunkHeight + blockHeight;
-	        	metadata[blockIndex] = 0;
-	        	if(blockHeight == blockMaxHeight)
-	        	{
-	        		blocks[blockIndex] = aincradGrass;
-	        	}
-	        	else if(blockHeight < blockMaxHeight && blockHeight >= 40)
-	        	{
-	        		blocks[blockIndex] = aincradDirt;
-	        	}
-	        	else if(blockHeight < 40 && blockHeight >=0 && isWithinCone(distance, blockHeight))
-	        	{
-	        		blocks[blockIndex] = aincradDirt;
-	        	}
-	        	else
-	        	{
-	        		blocks[blockIndex] = Blocks.air;
-	        	}
-            }
-        }
-		if(distance <= floor2WallEnd && distance > floor2WallStart)
-		{
-			int floor2WallTop = ((wallThickness-(floor2WallEnd - distance)) * wallSegmentHeight) + floor2HeightStart;
-			int floor2WallBottom = floor2WallTop-wallSegmentHeight;
-			
-			for (int blockHeight = 255; blockHeight >= 0; --blockHeight)
-	        {
-				int blockIndex = (chunkZ * 16 + chunkX) * chunkHeight + blockHeight;
-	        	metadata[blockIndex] = 0;
-	        	if(blockHeight <= floor2WallTop && blockHeight >= floor2WallBottom)
-	        	{
-	        		blocks[blockIndex] = this.wallBlock;
-	        	}
-	        }
-		}
-		else if(distance <= floor2WallStart)
-		{
-			Block aincradGrass = this.topBlock;
-	        byte blockData = (byte)(this.field_150604_aj & 255);
-	        Block aincradDirt = this.fillerBlock;
-	        int noiseGenRand = (int)Math.pow(noiseGenSeed / 3.0D + 3.0D, 1.8D);
-	        int blockMaxHeight = noiseGenRand + 5 + floor2HeightStart;
-	        for (int blockHeight = 255; blockHeight >= 0; --blockHeight)
-	        {
-	        	int blockIndex = (chunkZ * 16 + chunkX) * chunkHeight + blockHeight;
-	        	metadata[blockIndex] = 0;
-	        	if(blockHeight == blockMaxHeight)
-	        	{
-	        		blocks[blockIndex] = aincradGrass;
-	        	}
-	        	else if(blockHeight < blockMaxHeight && blockHeight >= floor2HeightStart)
-	        	{
-	        		blocks[blockIndex] = aincradDirt;
-	        	}
+		        Block aincradGrass = this.topBlock;
+		        byte blockData = (byte)(this.field_150604_aj & 255);
+		        Block aincradDirt = this.fillerBlock;
+		        int blockMaxHeight = noiseGenRand + 40;
+		        for (int blockHeight = 255; blockHeight >= 0; --blockHeight)
+		        {
+		        	int blockIndex = (chunkZ * 16 + chunkX) * chunkHeight + blockHeight;
+		        	metadata[blockIndex] = 0;
+		        	if(blockHeight == blockMaxHeight)
+		        	{
+		        		blocks[blockIndex] = aincradGrass;
+		        	}
+		        	else if(blockHeight < blockMaxHeight && blockHeight >= 40)
+		        	{
+		        		blocks[blockIndex] = aincradDirt;
+		        	}
+		        	else if(blockHeight < 40 && blockHeight >=0 && isWithinCone(distance, blockHeight))
+		        	{
+		        		blocks[blockIndex] = aincradDirt;
+		        	}
+		        	else
+		        	{
+		        		blocks[blockIndex] = Blocks.air;
+		        	}
+	            }
 	        }
 		}
     }
