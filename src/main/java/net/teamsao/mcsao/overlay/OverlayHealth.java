3e64f136414f5f6f551a322373cdd89eac0773aa
Sirpwnsamin <ryangreen105@gmail.com>
2014-10-22 18:10:25 -0400

    Health Overlay
    "Finished" the health overlay (additional tweaking is a possibility)

--- src/main/java/net/teamsao/mcsao/overlay/OverlayHealth.java
+++ src/main/java/net/teamsao/mcsao/overlay/OverlayHealth.java
@@ -4,11 +4,13 @@ import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.teamsao.mcsao.helper.Reference;
 import net.teamsao.mcsao.player.PlayerSAO;
+
 import org.lwjgl.opengl.GL11;
 
 import java.util.Iterator;
@@ -17,9 +19,14 @@ import java.util.Iterator;
  * <p>OverlayHealth implements the health bar from Sword Art Online.</p>
  * @see <a href="http://www.minecraftforge.net/wiki/Gui_Overlay">Gui Overlay - MCF Wiki</a>
  * @author 5chris100
+ * @author SirPwn
  */
-public class OverlayHealth extends Gui {
+public class OverlayHealth extends GuiScreen {
     private static Minecraft mc;
+    
+    public static ResourceLocation textureBase = new ResourceLocation(Reference.MODID.toLowerCase(), "/textures/overlay/health/healthBarBase.png");
+    public static ResourceLocation textureBar = new ResourceLocation(Reference.MODID.toLowerCase(), "/textures/overlay/health/healthBar.png");
+    public static final ResourceLocation originalIcons = new ResourceLocation("textures/gui/icons.png");
 
     public OverlayHealth(Minecraft mc) {
         super();
@@ -35,45 +42,56 @@ public class OverlayHealth extends Gui {
         if (evt.isCancelable() || evt.type != RenderGameOverlayEvent.ElementType.HEALTH) {
             return;
         }
-        int xPos = 2; // Horizontal position of the overlay relative to the top-left corner.
-        int yPos = 15; // Vertical position of the overlay relative to the top-left corner.
-        int hpxPos = 12;
-        int hpyPos = 20;
-        int hpLength = 80;
+        double xPos = 5; // Horizontal position of the overlay relative to the top-left corner.
+        double yPos = 5; // Vertical position of the overlay relative to the top-left corner.
+        double frameXSize = 100;
+        double frameYSize = 20;
+        double barXSize = 58.5;
+        double barYSize = 6;
         float health = mc.thePlayer.getHealth();
-        if (health > 0) {
+        if (health > 0 && mc.thePlayer.worldObj.provider.dimensionId == 2) {
             // The player is alive! TODO: Let's draw the health bar!
-            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-            GL11.glDisable(GL11.GL_LIGHTING);
-            mc.renderEngine.bindTexture(new ResourceLocation("sao", "/assets/sao/textures/overlay/health/healthbase.png"));
-            Tessellator tess = Tessellator.instance;
-
-            this.drawTexturedModalRect(xPos, yPos, 0, 0, 100, 20);
 
             PlayerSAO playerProp = PlayerSAO.get(mc.thePlayer);
 
-            GL11.glDisable(GL11.GL_TEXTURE_2D);
-
             //int precent = playerProp.getHealth()/playerProp.getMaxHealth();
-            float precent = health/ mc.thePlayer.getMaxHealth();
-            tess.startDrawingQuads();
-            if(precent*100 < 33){
-                tess.setColorRGBA(255, 0, 0, 255);
-            }else if(precent*100 < 67){
-                tess.setColorRGBA(250,250, 0, 255);
-            }else{
-                tess.setColorRGBA(0, 255, 0, 255);
-            }
-            tess.addVertex(hpxPos+0, hpyPos+10, this.zLevel);
-            tess.addVertex(hpxPos+(hpLength*precent), hpyPos+10, this.zLevel);
-            tess.addVertex(hpxPos+(hpLength*precent), hpyPos+0, this.zLevel);
-            tess.addVertex(hpxPos+0, hpyPos+0, this.zLevel);
-            tess.draw();
-
-            GL11.glEnable(GL11.GL_TEXTURE_2D);
-
-
+            double percent = health/ mc.thePlayer.getMaxHealth();
+            drawFrame(xPos, yPos, frameXSize, frameYSize);
+            drawBar(percent, xPos + 34, yPos + 4.25, barXSize, barYSize);
         }
+        
     }
-
+    
+    public void drawBar(double percent, double xPos, double yPos, double xSize, double ySize) {
+    	mc.renderEngine.bindTexture(textureBar);
+    	GL11.glPushMatrix();
+        GL11.glColor4f(1.5F - (float) percent, (float) percent, 0.1F, 0.75F);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glEnable(GL11.GL_BLEND);
+        Tessellator t = Tessellator.instance;
+        t.startDrawingQuads();
+        t.addVertexWithUV(xPos, yPos, this.zLevel, 0, 0);
+        t.addVertexWithUV(xPos, yPos + ySize, this.zLevel, 0, 1);
+        t.addVertexWithUV(xPos + (xSize * percent), yPos + ySize, this.zLevel, 1*percent, 1);
+        t.addVertexWithUV(xPos + (xSize * percent), yPos, this.zLevel, 1*percent, 0);
+        t.draw();
+        GL11.glPopMatrix();
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        mc.renderEngine.bindTexture(originalIcons);
+    }
+    
+    public void drawFrame(double xPos, double yPos, double xSize, double ySize) {
+    	mc.renderEngine.bindTexture(textureBase);
+    	GL11.glPushMatrix();
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        Tessellator t = Tessellator.instance;
+        t.startDrawingQuads();
+        t.addVertexWithUV(xPos, yPos, this.zLevel, 0, 0);
+        t.addVertexWithUV(xPos, yPos + ySize, this.zLevel, 0, 1);
+        t.addVertexWithUV(xPos + xSize, yPos + ySize, this.zLevel, 1, 1);
+        t.addVertexWithUV(xPos + xSize, yPos, this.zLevel, 1, 0);
+        t.draw();
+        GL11.glPopMatrix();
+    }
 }
